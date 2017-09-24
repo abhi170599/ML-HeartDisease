59cab570cba0f861f12db2bd7b342ec3982ffed3
Azda Firmansyah <azda.duplikat@gmail.com>
2017-09-24 15:48:58 +0700

    Update HeartDiseasePredict.py

--- HeartDiseasePredict.py
+++ HeartDiseasePredict.py
@@ -3,7 +3,7 @@ Created on Sep 23, 2017
 
 @author: Azda Firmansyah
 '''
-from ExerciseHeartDisease.DataHelper import datasetHeartDisease
+from DataHelper import datasetHeartDisease
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
@@ -16,9 +16,7 @@ X_features = data[0]
 Y_labels = data[1]
 
 X_train, X_test, Y_train, Y_test = train_test_split(X_features, Y_labels, test_size=0.1, random_state=100)
-#print(Y_test)
-#print("Test Data : ",X_test)
-#X_test=[[48,1,2,110,229,0,0,168,0,1,3,0,7]]
+#X_test=[[48,1,2,110,229,0,0,168,0,1,3,0,7]] You can use your data
 
 print("===Decision Tree Classifier===")
 tree_clf = DecisionTreeClassifier(min_samples_split=60)
@@ -59,32 +57,3 @@ pred_randfor = randfor_clf.predict(X_test)
 print("Predict Random Forest :",pred_randfor)
 score_randfor = accuracy_score(Y_test, pred_randfor)
 print("Score Random Forest :",score_randfor)
-"""
-Attribute Information:
-      -- 1. (age)       
-      -- 2. #4  (sex)       
-      -- 3. #9  (cp)        
-      -- 4. #10 (trestbps)  
-      -- 5. #12 (chol)      
-      -- 6. #16 (fbs)       
-      -- 7. #19 (restecg)   
-      -- 8. #32 (thalach)   
-      -- 9. #38 (exang)     
-      -- 10. #40 (oldpeak)   
-      -- 11. #41 (slope)     
-      -- 12. #44 (ca)        
-      -- 13. #51 (thal)
-      More information visit : https://archive.ics.uci.edu/ml/machine-learning-databases/heart-disease/heart-disease.names
-
-#X_test=[[48,1,2,110,229,0,0,168,0,1,3,0,7]]
-#X_test=[[48,1,2,110,229,0,0,168,0,1,3,0,7]]
-features_train, labels_train, features_test, labels_test = train_test_split(X,Y, test_size=0.3, random_state=42)
-
-#Tree Clf
-tree_clf = DecisionTreeClassifier()
-tree_clf.fit(X, Y)
-tree_predict = tree_clf.predict(features_test)
-print(tree_predict)
-"""
-#tree_score = accuracy_score(labels_test, tree_predict)
-#print(tree_score)

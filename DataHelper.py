ed418ff17a8c1114aa26143ed26f8b06f342630e
Azda Firmansyah <azda.duplikat@gmail.com>
2017-09-24 15:47:12 +0700

    Update DataHelper.py

--- DataHelper.py
+++ DataHelper.py
@@ -8,7 +8,7 @@ import csv
 #1st way use numpy
 
 def datasetHeartDisease():
-    file_path = '../Data/HeartDisease.csv'
+    file_path = 'HeartDisease.csv'
     with open(file_path,encoding='utf-8') as csv_file:
         data_file = csv.reader(csv_file)
         first_line = next(data_file)
@@ -21,32 +21,3 @@ def datasetHeartDisease():
             data[count] = np.asarray(value[:-1], dtype=np.float64)
             target[count] = np.asarray(value[-1], dtype=np.int)        
     return data, target 
-#datasetHeartDisease()    
-    #file_path_labels = '../Data/HeartDiseaseLabels.csv'
-
-"""    
-def getFeaturesHeartDisease(): 
-    file_path = '../Data/HeartDiseaseFeatures.csv'   
-    raw_data = open(file_path,encoding='utf-8')
-    data = np.loadtxt(raw_data, delimiter=',')
-    return data
-
-def getLabelsHeartDisease():
-    file_path = '../Data/HeartDiseaseLabels.csv'
-    raw_data = open(file_path,encoding='utf-8')
-    data = np.loadtxt(raw_data)
-    return data
-
-#2nd way open file
-def useStandard():
-    with open(file_path, encoding='utf-8') as csv_file:
-        data_file = csv.reader(csv_file)
-        list_data = list(data_file)
-        print("Number of Rows :",len(list_data))
-        print("Number of Cols :",len(list_data[0]))
-        #for i,items in enumerate(data_file):            
-        #    print(items)
-useStandard()        
-npData = useNp()
-print(npData.shape)
-"""
